5461bf7e05189e9fc9c2f9aa117319863794f657
eXory2024 <exory@anio.software>
2024-04-04 20:10:51 +0200

    Replace @anio-node-foundation/fs-api with @anio-fs/api.

--- src/index.mjs
+++ src/index.mjs
@@ -1,21 +1,18 @@
-import createFSObject from "@anio-node-foundation/fs-api"
+import fs from "@anio-fs/api"
 
 import sync_impl from "./auto/sync.mjs"
 import async_impl from "./auto/async.mjs"
 
-const async_fs = createFSObject({sync: false})
-const sync_fs = createFSObject({sync: true})
-
 async function nodeFsWriteAtomicFile(file_path, data, {
 	create_parents = false
 } = {}) {
-	return await async_impl(async_fs, file_path, data, {create_parents})
+	return await async_impl(fs.async, file_path, data, {create_parents})
 }
 
 nodeFsWriteAtomicFile.sync = function(file_path, data, {
 	create_parents = false
 } = {}) {
-	return sync_impl(sync_fs, file_path, data, {create_parents})
+	return sync_impl(fs.sync, file_path, data, {create_parents})
 }
 
 export default nodeFsWriteAtomicFile
